dcdaf1a794d30edea9593458f726203c0865f820
Janani2295 <jananivinoth1709@gmail.com>
2025-04-09 21:04:57 +0530

    Sample pgm

---
+++ src/test/java/com/sample/SampleGit.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+public class SampleGit {
+	public void m1() {
+        System.out.println("m1");
+	}
+	public void m2() {
+		System.out.println("M2");
+	}
+	public static void main(String[] args) {
+		SampleGit sg=new SampleGit();
+		sg.m1();
+		sg.m2();
+	}
+
+}
